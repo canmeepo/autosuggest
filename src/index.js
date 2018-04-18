5bdb123fc395385b3be0091a4bac73cec600ad38
canmeepo <canmeepo@gmail.com>
2018-04-18 23:23:24 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/styles/index.styl';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(reducers, {});
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    suggestions: null,
+});
+
+export default rootReducer;
